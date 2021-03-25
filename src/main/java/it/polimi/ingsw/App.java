bc31e5d92d0042a9d83db1a4d41e274ca60cdad6
Alessandro De Luca <alessandro2.deluca@mail.polimi.it>
2021-03-25 10:31:56 +0100

    Testing commit function on a default project

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Trying to test commits, push and pulls");
+    }
+}
