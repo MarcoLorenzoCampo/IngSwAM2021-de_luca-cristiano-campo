eb54d9cc856570e0faf465efaf41d7bd95200181
Alessandro De Luca <alessandro2.deluca@mail.polimi.it>
2021-04-11 12:45:51 +0200

    Added jvadoc comments

--- src/main/java/it/polimi/ingsw/model/warehouse/Warehouse.java
+++ src/main/java/it/polimi/ingsw/model/warehouse/Warehouse.java
@@ -13,6 +13,10 @@ import java.util.stream.Collectors;
 public class Warehouse {
     private ArrayList<Shelf> shelves;
 
+    /**
+     * warehouse constructor, initializes shelves as an arraylist of size 3 with all elements as
+     * UNDEFINED with 0 quantity, this means that the slot can be filled
+     */
     public Warehouse (){
         shelves = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
@@ -24,10 +28,21 @@ public class Warehouse {
         return shelves;
     }
 
+    /**
+     *
+     * @param type -- given by the ExtraInventoryLeader card, states what type of resource the new
+     *                shelf needs to be
+     */
     public void addExtraInventory(ResourceType type){
         shelves.add(new Shelf(type, 2));
     }
 
+    /**
+     *
+     * @return -- true if warehouse follows the rule of the game which are
+     *            1) no two shelves can  hold the same type of resource (unless one of them comes from a leader card)
+     *            2) each shelf must hold a quantity that is less or equal its maximum capacity
+     */
     public boolean isWarehouseValid(){
         Map<ResourceType, Integer> validation = new HashMap<>();
         for (int i = 0; i < 3; i++) {
@@ -54,6 +69,11 @@ public class Warehouse {
         return true;
     }
 
+    /**
+     *
+     * @param desired -- desired type of resource that whose presence in the shelves needs to be checked
+     * @return -- states if at least one instance of that resource is found in the warehouse
+     */
     public boolean checkPresence(ResourceType desired){
         Optional<Shelf> check =
                 shelves.stream()
@@ -62,6 +82,10 @@ public class Warehouse {
         return check.isPresent();
     }
 
+    /**
+     * sorts shelves based on the quantity they hold, this guarantees that the player will always
+     * have the optimum arrangement of resources whenever they add a new one
+     */
     public void sortShelves(){
 
         ArrayList<Shelf> clonedShelves = new ArrayList<>(shelves.size());
@@ -80,12 +104,24 @@ public class Warehouse {
         }
     }
 
+    /**
+     * if the quantity of a shelf is 0, it sets its type to UNDEFINED stating
+     * that it can hold any resource
+     */
     public void clearShelves(){
         for (int i = 0; i < 3; i++) {
             shelves.get(i).clear();
         }
     }
 
+    /**
+     *
+     * @param currentShelf -- shelf in which we are trying to add a new resource
+     * @return -- states if the resource was correctly placed or not
+     * if the shelf is not extra then we need to check if any swap is available to let it hold another resource
+     * if the shelf is extra then we need to check if there is a normal shelf of the same type
+     * that can hold the resource or check for an empty slot to place the resource
+     */
     public boolean tryAlternative(Shelf currentShelf){
         int index = shelves.indexOf(currentShelf);
         if(index < 2){
@@ -130,6 +166,12 @@ public class Warehouse {
         else return false;
     }
 
+
+    /**
+     *
+     * @param input -- resource that needs to be placed
+     * @return -- states if the resource was correctly placed in an empty slot or not
+     */
     public boolean placeInEmpty(MaterialResource input){
 
         Optional <Shelf> temp =
@@ -144,7 +186,14 @@ public class Warehouse {
         else return false;
     }
 
-
+    /**
+     *
+     * @param input -- resource that the player wants to place in their warehouse
+     * @throws DiscardResourceException -- exception that states that the current state of the warehouse+
+     *                                     cannot hold any more of the resource in input, thus signaling
+     *                                     to the player that any further instance of said resource needs
+     *                                     to be discarded
+     */
     public void addResource (MaterialResource input) throws DiscardResourceException {
         if (checkPresence(input.getResourceType())){
             boolean placed = false;
@@ -184,6 +233,16 @@ public class Warehouse {
         }
     }
 
+    /**
+     *
+     * @param tag - type and quantity of resource that wants to be removed from warehouse
+     * @throws CannotRemoveResourceException -- exception that states that the request could not be
+     *                                         executed, how much of the request is left
+     *                                         (meaning that the request was either not executed at all
+     *                                         or partially executed) signaling the player that the remaining
+     *                                         quantity needs to be removed from somewhere else
+     *                                         or that the request will never be fulfilled
+     */
     public void removeResources(ResourceTag tag) throws CannotRemoveResourceException {
         if(checkPresence(tag.getType())){
             int quantity = tag.getQuantity();
