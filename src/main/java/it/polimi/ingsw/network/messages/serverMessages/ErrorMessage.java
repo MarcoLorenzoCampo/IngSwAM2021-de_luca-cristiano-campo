9ddbbe95dee9e29e356d26a6d032a8cd7a2bc925
MarcoLorenzoCampo <marcolorenzo.campo@mail.polimi.it>
2021-05-21 11:42:29 +0200

    Improved connection conditions

--- src/main/java/it/polimi/ingsw/network/messages/serverMessages/ErrorMessage.java
+++ src/main/java/it/polimi/ingsw/network/messages/serverMessages/ErrorMessage.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.network.messages.serverMessages;
 import it.polimi.ingsw.enumerations.PossibleMessages;
 import it.polimi.ingsw.network.messages.Message;
 
+/**
+ * Error message sent by the server whenever something goes wrong during the
+ * connection phase of the game.
+ */
 public class ErrorMessage extends Message {
     private static final long serialVersionUID = -3513490851483807040L;
 
