066a0dd7ddfe3239a2fe69f58e2727d3bc093682
MarcoLorenzoCampo <marcolorenzo.campo@mail.polimi.it>
2021-05-30 19:03:45 +0200

    Included shade plugin, fixed pom dependencies and functioning jar

--- src/main/java/it/polimi/ingsw/parsers/ServerConfigParser.java
+++ src/main/java/it/polimi/ingsw/parsers/ServerConfigParser.java
@@ -3,24 +3,22 @@ package it.polimi.ingsw.parsers;
 import com.google.gson.Gson;
 import it.polimi.ingsw.network.utilities.ServerConfigPOJO;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public final class ServerConfigParser {
 
-    private static final String serverConfigPath = "src/main/resources/server_config.json";
+    private static final String serverConfigPath = "/server_config.json";
 
     public static ServerConfigPOJO readServerConfig() {
 
         Gson gson = new Gson();
-        FileReader reader = null;
+        Reader reader;
 
-        try {
-            reader = new FileReader(serverConfigPath);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        assert reader != null;
+        reader = new InputStreamReader(Objects.requireNonNull(ServerConfigParser.class.getResourceAsStream(serverConfigPath)),
+                StandardCharsets.UTF_8);
 
         return gson.fromJson(reader, ServerConfigPOJO.class);
     }
