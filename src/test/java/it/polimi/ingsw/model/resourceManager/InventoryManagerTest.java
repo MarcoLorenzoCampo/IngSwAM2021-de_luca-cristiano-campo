b6353a4831572c9c7af3f04e207710e4981d2858
Alessandro De Luca <alessandro2.deluca@mail.polimi.it>
2021-06-30 02:50:13 +0200

    tests for inventoryManager, strongbox and productionBoard done

--- src/test/java/it/polimi/ingsw/model/resourceManager/InventoryManagerTest.java
+++ src/test/java/it/polimi/ingsw/model/resourceManager/InventoryManagerTest.java
@@ -1,17 +1,19 @@
 package it.polimi.ingsw.model.resourceManager;
 
+
 import it.polimi.ingsw.enumerations.ResourceType;
+import it.polimi.ingsw.exceptions.CannotRemoveResourceException;
 import it.polimi.ingsw.exceptions.DiscardResourceException;
 import it.polimi.ingsw.model.game.PlayingGame;
 import it.polimi.ingsw.model.inventoryManager.InventoryManager;
 import it.polimi.ingsw.model.player.RealPlayer;
 import it.polimi.ingsw.model.utilities.MaterialResource;
 import it.polimi.ingsw.model.utilities.ResourceTag;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -116,8 +118,8 @@ class InventoryManagerTest {
         inventoryManager.getWarehouse().getShelves().get(2).setElement(new ResourceTag(ResourceType.SHIELD, 2));
 
         //Act
-        assertDoesNotThrow(()->{inventoryManager.addResourceToWarehouse(0);});
-        DiscardResourceException exception = assertThrows(DiscardResourceException.class,  ()->{inventoryManager.addResourceToWarehouse(0);});
+        assertDoesNotThrow(()-> inventoryManager.addResourceToWarehouse(0));
+        DiscardResourceException exception = assertThrows(DiscardResourceException.class,  ()-> inventoryManager.addResourceToWarehouse(0));
         //simulating the action resolves the exception
         inventoryManager.removeFromBuffer(0);
 
@@ -150,13 +152,141 @@ class InventoryManagerTest {
 
     @Test
     void discountPrice() {
+        //Arrange
+        List<ResourceTag> price = new ArrayList<>();
+        price.add(new ResourceTag(ResourceType.COIN, 3));
+        price.add(new ResourceTag(ResourceType.SHIELD, 3));
+        price.add(new ResourceTag(ResourceType.SERVANT, 3));
+
+        //Act
+        inventoryManager.addDiscountLeader(ResourceType.COIN);
+        inventoryManager.addDiscountLeader(ResourceType.SHIELD);
+        inventoryManager.applyDiscount(price);
+
+        //Assert
+        assertAll(
+                () -> assertEquals(ResourceType.COIN, inventoryManager.getDiscount().get(0)),
+                () -> assertEquals(2, price.get(0).getQuantity()),
+                () -> assertEquals(ResourceType.COIN, price.get(0).getType()),
+                () -> assertEquals(2, price.get(1).getQuantity()),
+                () -> assertEquals(ResourceType.SHIELD, price.get(1).getType()),
+                () -> assertEquals(3, price.get(2).getQuantity()),
+                () -> assertEquals(ResourceType.SERVANT, price.get(2).getType())
+        );
+
     }
 
     @Test
     void removeFromWarehouse() {
+        //Arrange
+        inventoryManager.getInventory().put(ResourceType.SHIELD, 2);
+        inventoryManager.getWarehouse().getShelves().get(2).setElement(new ResourceTag(ResourceType.SHIELD, 2));
+
+        //Act
+        assertDoesNotThrow(()-> inventoryManager.removeFromWarehouse(new ResourceTag(ResourceType.SHIELD, 1)));
+        CannotRemoveResourceException exception = assertThrows(CannotRemoveResourceException.class,  ()-> inventoryManager.removeFromWarehouse(new ResourceTag(ResourceType.SHIELD, 2)));
+
+        //Assert
+        assertAll(
+                () -> assertFalse( inventoryManager.getWarehouse().checkPresence(ResourceType.SHIELD)),
+                //no one calls the update when remove from warehouse isn't paired with the other
+                () -> assertEquals(1, inventoryManager.getInventory().get(ResourceType.SHIELD)),
+                () -> assertEquals(1, exception.getQuantity())
+        );
     }
 
     @Test
     void removeFromStrongbox() {
+        //Arrange
+        inventoryManager.getStrongbox().getInventory().put(ResourceType.SHIELD, 2);
+        inventoryManager.getInventory().put(ResourceType.SHIELD, 2);
+        //Act
+        assertDoesNotThrow(()-> inventoryManager.removeFromStrongbox(new ResourceTag(ResourceType.SHIELD, 1)));
+        CannotRemoveResourceException exception = assertThrows(CannotRemoveResourceException.class,  ()-> inventoryManager.removeFromStrongbox(new ResourceTag(ResourceType.SHIELD, 2)));
+
+        //Assert
+        assertAll(
+                () -> assertEquals(0,inventoryManager.getStrongbox().getInventory().get(ResourceType.SHIELD)),
+                //no one calls the update when remove from warehouse isn't paired with the other
+                () -> assertEquals(1, inventoryManager.getInventory().get(ResourceType.SHIELD)),
+                () -> assertEquals(1, exception.getQuantity())
+        );
+    }
+
+    @Test
+    void DeleteWhite(){
+        //Arrange
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.COIN));
+        int pre_size;
+
+        //Act
+        pre_size = inventoryManager.getBuffer().size();
+        inventoryManager.whiteMarblesExchange();
+
+        //Assert
+        assertAll(
+                () -> assertNotEquals(pre_size, inventoryManager.getBuffer().size()),
+                () -> assertEquals(1, inventoryManager.getBuffer().size()),
+                () -> assertEquals(ResourceType.COIN, inventoryManager.getBuffer().get(0).getResourceType())
+        );
+
+
+    }
+
+    @Test
+    void ChangeOnlyOne(){
+        //Arrange
+        inventoryManager.addExchangeLeader(ResourceType.SHIELD);
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.COIN));
+        int pre_size;
+
+        //Act
+        pre_size = inventoryManager.getBuffer().size();
+        inventoryManager.whiteMarblesExchange();
+
+        //Assert
+        assertAll(
+                () -> assertEquals(pre_size, inventoryManager.getBuffer().size()),
+                () -> assertEquals(4, inventoryManager.getBuffer().size()),
+                () -> assertEquals(ResourceType.SHIELD, inventoryManager.getBuffer().get(0).getResourceType()),
+                () -> assertEquals(ResourceType.SHIELD, inventoryManager.getBuffer().get(1).getResourceType()),
+                () -> assertEquals(ResourceType.SHIELD, inventoryManager.getBuffer().get(2).getResourceType()),
+                () -> assertEquals(ResourceType.COIN, inventoryManager.getBuffer().get(3).getResourceType())
+        );
+    }
+
+    @Test
+    void ChangeChoice(){
+        //Arrange
+        inventoryManager.addExchangeLeader(ResourceType.SHIELD);
+        inventoryManager.addExchangeLeader(ResourceType.COIN);
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.UNDEFINED));
+        inventoryManager.deposit(new MaterialResource(ResourceType.COIN));
+        int pre_size;
+
+        //Act
+        pre_size = inventoryManager.getBuffer().size();
+        inventoryManager.customExchange(0, ResourceType.SHIELD);
+        inventoryManager.customExchange(1, ResourceType.COIN);
+        inventoryManager.customExchange(2, ResourceType.STONE);
+
+        //Assert
+        assertAll(
+                () -> assertEquals(pre_size, inventoryManager.getBuffer().size()),
+                () -> assertEquals(4, inventoryManager.getBuffer().size()),
+                () -> assertEquals(ResourceType.SHIELD, inventoryManager.getBuffer().get(0).getResourceType()),
+                () -> assertEquals(ResourceType.COIN, inventoryManager.getBuffer().get(1).getResourceType()),
+                () -> assertEquals(ResourceType.UNDEFINED, inventoryManager.getBuffer().get(2).getResourceType()),
+                () -> assertEquals(ResourceType.COIN, inventoryManager.getBuffer().get(3).getResourceType())
+        );
+
     }
 }
